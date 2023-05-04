d9b559dcd30cef96fa08aae26366470e5b073aa5
Valdemar <valba546@student.liu.se>
2023-05-04 14:22:48 +0200

    updated text

--- javascript/main.js
+++ javascript/main.js
@@ -5,35 +5,36 @@ let selectedBubble = null;
 
 /* Statements */
 const statements = [
-    "I never left the rebellious phase from my childhood.",
+    "You never left the rebellious phase from your childhood.",
     "If someone tells you to calm down you do the opposite and go crazy",
-    "I laugh louder and more often then 99.99% of my surroundings.",
-    "I love fika more than i love life.",
-    "I'm not ashamed of anything, and if I am I'm not.",
-    "My money is best spend on new wood work tools, no matter the price.",
-    "If something (fika) is free I am taking more than is socially acceptable.",
+    "You like to build things.",
+    "You laugh louder and more often than 99.99% of your surroundings.",
+    "A sandwich is supposed to consist of more butter than bread.",
+    "You're not ashamed of anything, and if you are, you're not.",
+    "Green is undoubtedly the best color. GREEN GREEN GREEEEEEEN!",
+    "Money is best spent on new wood work tools, no matter the price.",
+    "If fika is free you are taking more than is socially acceptable.",
     "Your partner is EXTREMELY handsome, smart, generous and kind. Most likely one of the best people ever to live.",
+    "To party hard means falling asleep around 23:30 during a conversation or in a club toilet.",
+    "You eat vegetarian food (except fish, coz fish is basically sallad) to save the environment but travel by plane all over the world all the time.",
     "You are surrounded by incredible people from all around the world who loves you.",
-    "I am a overly loving, kindhearted creative genius with more karma than I can spend that.",
-    "I can sleep anywhere, and anytime in any position.",
-    "It is supposed to be more butter than bread on a sandwich.",
-    "I like to build things",
-    "Green IS the best color"
+    "You are an overly loving, kind hearted, creative genius with more karma than you can spend."
 ];
+
 
 /* Results */
 const results = [
-    "0-10% = You are nothing like Nanny.",
-    "10-20% = You share a few traits with Nanny, but not much.",
-    "20-30% = You have some similarities with Nanny, but there's room for improvement.",
-    "30-40% = You're a bit like Nanny, but there's still work to be done.",
-    "40-50% = You're somewhat similar to Nanny.",
-    "50-60% = You share a fair amount of traits with Nanny.",
-    "60-70% = You have a lot in common with Nanny.",
-    "70-80% = You're very much like Nanny.",
-    "80-90% = You're almost like Nanny, just a few things to work on.",
-    "90-99% = You're very close to being like Nanny.",
-    "100% = Wow, congratulations you are 100% Nanny. You probably life life to the fullest and are surrounded by the most incredible people."
+    "0-10% = Oh no! You are less than 10% Nanny. You should probably eat some more fish, travel to an Indian wedding and make new awesome friends at this party!",
+    "10-20% =That's a low Nanny percentage… You should eat some more butter, buy an expensive screwdriver and make some new awesome friends at this party!",
+    "20-30% = There is still time! Make some new wonderful friends at this party, take an extra large portion of cake and fall asleep in the toilet to increase your Nannyness.",
+    "30-40% = Only one third Nanny in you! Unless you want do die young and broke you should really take some extra cake and make some new wonderful friends at this party",
+    "40-50% = There is some Nannyness in you! There could be more… take that extra piece of cake and scream at someone for telling you to calm down!",
+    "50-60% = Alright alright, fifty/fifty, half Nanny, half boring! Keep the Nannyness flowing and eat more cake!",
+    "60-70% = Wow! Two thirds Nanny! You could go on and make president of the woodworkers united one day! Keep it going!",
+    "70-80% = You're almost a Nanny by default! People must really love you! Stay unhinged and sexy!",
+    "80-90% = Wow! That's a high percentage! Keep spreading that butter! You will live a long and happy life (maybe buy some more drills and allen keys just to be on the safe side)",
+    "90-99% =Congratulations, you are part of the top 10% of Nanny! One could almost believe that you are the actual Nanny! You probably live a wonderful life working as a traveling carpenter eating enormous amounts of fika each day.",
+    "100% = Wow! Congratulations you are 100% Nanny! You probably live life to the fullest and are surrounded by the most incredible people! Keep doing what you are doing!"
 ];
 
 /* Functions */
@@ -41,9 +42,9 @@ function setupStatement() {
     //  Set up the progress bar
     const fill = document.querySelector('.fill');
     const percent = document.querySelector('.percent');
-    
+
     const percentage = Math.floor((currStatement / statements.length) * 100);
-    
+
     fill.style.width = percentage + '%';
     percent.textContent = percentage + '%';
     fill.style.backgroundColor = 'green';
@@ -51,17 +52,32 @@ function setupStatement() {
     // Set up the current statement
     var statementText = document.getElementById("statement");
     statementText.innerText = statements[currStatement];
+
+    // Check if statement is singel question
+    if (currStatement == 9) {
+        var buttonText = document.getElementById("button-text");
+        buttonText.textContent = "Singel and ready to mingle?";
+    }
+    else if (currStatement == 10) {
+        var buttonText = document.getElementById("button-text");
+        buttonText.textContent = "Next question";
+    }
 }
 
 function toggleCheckmark(indexBubble) {
     var bubbles = document.getElementsByClassName("bubble");
-    if(selectedBubble !== null) {
+    if (selectedBubble !== null) {
         // Reset the previous selected bubble
         bubbles[selectedBubble].classList.toggle("selected");
         bubbles[selectedBubble].style.backgroundColor = "transparent";
         // If the same bubble is selected, unselect it
-        if(selectedBubble === indexBubble) {
+        if (selectedBubble === indexBubble) {
             selectedBubble = null;
+            // Change text for next button
+            if (currStatement == 9) {
+                var buttonText = document.getElementById("button-text");
+                buttonText.textContent = "Singel and ready to mingle?";
+            }
             return;
         }
     }
@@ -70,17 +86,26 @@ function toggleCheckmark(indexBubble) {
     var style = getComputedStyle(bubbles[indexBubble]);
     bubbles[indexBubble].style.backgroundColor = style.getPropertyValue("--border-color");
     selectedBubble = indexBubble;
+    // Change text for next button
+    if (currStatement == 9) {
+        var buttonText = document.getElementById("button-text");
+        buttonText.textContent = "Next question";
+    }
 }
 
 function nextStatement() {
+    // Non selected and single question we pass the question with full points
+    if (currStatement == 9 && selectedBubble == null) {
+        selectedBubble = 4;
+    }
     // Check if a bubble is selected
-    if (selectedBubble === null) {
+    else if (selectedBubble === null) {
         //alert("Please select a bubble");
         Swal.fire({
             title: 'Oops...',
             text: 'Please answer the statement/question by pressing a bubble.',
             confirmButtonColor: "#84fffe"
-          })
+        })
         return;
     }
 
